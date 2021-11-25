5eb267a9a97bde1b4e48d095d9e346ac9f239b1f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-25 15:02:55 +0530

    [Gyanendra] Add: A Program of UnOrdered List

---
+++ Data Structure Programs/Data Structure Programs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Data_Structure_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            Console.WriteLine("Welcome to Data Structure Programs Press 1-Un Ordered List, 2-Exit");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+            case 1:
+                    UnOrderedList.UnOrdered();
+                break;
+             case 2:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
